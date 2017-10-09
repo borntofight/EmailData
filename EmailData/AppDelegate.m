8394e68094183eb4b4ffdcb1a2c7ff1341395890
Andy Cao <geilility@126.com>
2017-10-09 17:49:34 +0800

    Update AppDelegate.m

--- EmailData/AppDelegate.m
+++ EmailData/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    
+    //!!
     [self initFiles];
     self.tabBarController = [[UITabBarController alloc] init];
     self.tabBarController.view.backgroundColor = [UIColor whiteColor];
